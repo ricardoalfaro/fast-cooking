f65e4ac44aae0609cb479cc74ce023b5818001de
Ricardo Alfaro <ricardoalfarog@gmail.com>
2026-08-13 13:15:55 -0400

    Improve recipe images and reading flow

--- sw.js
+++ sw.js
@@ -1,9 +1,10 @@
-const CACHE_NAME = 'fastcooking-v6';
+const CACHE_NAME = 'fastcooking-v7';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
-  './icon.svg'
+  './icon.svg',
+  './public/recipe-fallback.png'
 ];
 
 self.addEventListener('install', (event) => {
